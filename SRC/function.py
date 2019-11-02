50e02697a6c5488f430398d74298bac71eb70745
blancaalcala <blancaalcala97@gmail.com>
2019-11-02 14:57:24 +0100

    Sabado mañana

--- SRC/function.py
+++ SRC/function.py
@@ -1,6 +1,17 @@
 # write your functions here:
+import re 
 
 def renameF(df,column,old,new):
-    df[column] = df[column].replace(new, old)
+    df[column] = df[column].replace(old, new)
     return df[column]
 
+def findF(f_list,key_word,values):
+    for i in values:
+        if re.search(key_word,i):
+            f_list.append(i)
+    return f_list
+
+
+def find_Time(df,column,time,t,keyword,newword):
+    if re.search("(?i)"+keyword,time):
+            df[column][t] = newword
