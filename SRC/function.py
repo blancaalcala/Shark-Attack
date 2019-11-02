df83c73f22cd85c406cf4e650e526153cbfb32bf
blancaalcala <blancaalcala97@gmail.com>
2019-11-02 23:27:19 +0100

    sabado noche

--- SRC/function.py
+++ SRC/function.py
@@ -3,7 +3,6 @@ import re
 
 def renameF(df,column,old,new):
     df[column] = df[column].replace(old, new)
-    return df[column]
 
 def findF(f_list,key_word,values):
     for i in values:
@@ -15,3 +14,16 @@ def findF(f_list,key_word,values):
 def find_Time(df,column,time,t,keyword,newword):
     if re.search("(?i)"+keyword,time):
             df[column][t] = newword
+
+def Fatality(df,injuries):
+    fatal = ["fatal","kill","die","drown","remain","death","swept","recover"]
+    survive = ["injur","bitten","lacer","sever","surv","scrape","punc","marks","bit","cut","lost","serious","not recovered","wound","slash","Strip","bruise","abrasion"]
+    for i in range(0,len(injuries)):
+        for fat in fatal:
+            for surv in survive:
+                if re.search("(?i)"+fat,injuries[i]):
+                    df["Injury"][i] = "Fatal"
+                elif re.search("(?i)"+surv,injuries[i]):
+                    df["Injury"][i] = "Survived"
+                else:
+                    df["Injury"][i] = "Unknown"
